b0784e279fbf2c72bf10c67edd89ae27aa9992db
ShivaniSalavi <shivanisalavi20@gmail.com>
2023-04-26 18:37:16 +0530

    yessss

--- myfile.java
+++ myfile.java
@@ -1,5 +1,5 @@
     class Simple{  
         public static void main(String args[]){  
-         System.out.println("Hellooooo everyone this is Java");  
+         System.out.println("Helloooo everyone this is Java");  
         }  
     }  
