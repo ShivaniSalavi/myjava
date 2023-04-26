c7f5fbf81e12ce0e7e5ee998fa833c8d5f206068
ShivaniSalavi <shivanisalavi20@gmail.com>
2023-04-26 18:03:30 +0530

    newone commit

--- myfile.java
+++ myfile.java
@@ -1,5 +1,5 @@
     class Simple{  
         public static void main(String args[]){  
-         System.out.println("Hello everyone Java");  
+         System.out.println("Hellooooo everyone Java");  
         }  
     }  
